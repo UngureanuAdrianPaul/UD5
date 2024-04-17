5c0ab3566ca1941740ad9422582fa64426d0c84a
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-04-17 12:39:43 +0200

    inicio clase 17/04

--- src/AccesoConfiguracion.java
+++ src/AccesoConfiguracion.java
@@ -14,6 +14,21 @@ public class AccesoConfiguracion {
       lector = new FileInputStream(new File("./src/configuracion.properties"));
       propiedades.load(lector);
       propiedades.list(System.out);
+      System.out.println("***");
+      System.out.println(propiedades.get("jdbc"));
+      System.out.println(propiedades.get("usuario"));
+      System.out.println(propiedades.get("clave"));
+      
+      Conexion miConexion = new Conexion(
+         (String) propiedades.get("jdbc"), 
+         (String) propiedades.get("usuario"), 
+         (String) propiedades.get("clave")
+         );
+
+      System.out.println(miConexion.toString());   
+
+      
+      
    } catch (FileNotFoundException e) {
       System.out.println(e.getMessage());
    } finally {
