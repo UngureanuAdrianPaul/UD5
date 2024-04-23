c6501c2337a45103c83d4b28da32938f066e89f4
UngureanuAdrianPaul <adrianpaul.s371152@cesurformnacion.com>
2024-04-23 16:20:40 +0200

    Finalizado

--- src/AccesoConfiguracion.java
+++ src/AccesoConfiguracion.java
@@ -1,71 +1,115 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.File;
 
 public class AccesoConfiguracion {
- public static void main(String[] args) throws IOException {
-   Properties propiedades = new Properties();
-   
-   FileInputStream lector = null;
 
-   try {
-      lector = new FileInputStream(new File("./src/configuracion.properties"));
-      propiedades.load(lector);
-      propiedades.list(System.out);
-      System.out.println("***");
-      System.out.println(propiedades.get("jdbc"));
-      System.out.println(propiedades.get("usuario"));
-      System.out.println(propiedades.get("clave"));
-      
-      Conexion miConexion = new Conexion(
-         (String) propiedades.get("jdbc"), 
-         (String) propiedades.get("usuario"), 
-         (String) propiedades.get("clave")
-         );
+   public static void main(String[] args) throws IOException {
+      Properties propiedades = new Properties();
+      String pathFichero = "./src/configuracion.properties";
+      FileInputStream lector = null;
 
-      System.out.println(miConexion.toString());   
+      try {
+         lector = new FileInputStream(new File("./src/configuracion.properties"));
+         propiedades.load(lector);
+         System.out.println("***Visualizando con load y list***");
+         propiedades.list(System.out);
+         System.out.println("***Visualizando con get***");
+         System.out.println(propiedades.get("jdbc"));
+         System.out.println(propiedades.get("usuario"));
+         System.out.println(propiedades.get("clave"));
 
-      String nuevoJdbc;
-      String nuevoUsuario;
-      String nuevoClave;
-      Scanner teclado = new Scanner(System.in);
-      System.out.print("Introduce nuevo jdbc: ");
-      nuevoJdbc = teclado.nextLine();
-      System.out.print("Introduce nuevo usuario: ");
-      nuevoUsuario = teclado.nextLine();
-      System.out.print("Introduce nuevo clave: ");
-      nuevoClave = teclado.nextLine();
+         Conexion miConexion = new Conexion(
+               (String) propiedades.get("jdbc"),
+               (String) propiedades.get("usuario"),
+               (String) propiedades.get("clave"));
 
-      System.out.println("nuevo jdbc: "+nuevoJdbc);
-      System.out.println("nuevo usuario: "+nuevoUsuario);
-      System.out.println("nuevo clave: "+nuevoClave);
+         System.out.println("***Visualizando con conexión***");
+         System.out.println(miConexion);
 
-      Conexion nuevaConexion = new Conexion(nuevoJdbc, nuevoUsuario, nuevoClave);
+         String nuevoJdbc;
+         String nuevoUsuario;
+         String nuevoClave;
+         Scanner teclado = new Scanner(System.in);
+         System.out.print("Introduce nuevo jdbc: ");
+         nuevoJdbc = teclado.nextLine();
+         System.out.print("Introduce nuevo usuario: ");
+         nuevoUsuario = teclado.nextLine();
+         System.out.print("Introduce nuevo clave: ");
+         nuevoClave = teclado.nextLine();
 
-      insertarNuevaConfiguracion(nuevaConexion, propiedades);
-      propiedades.list(System.out);
+         System.out.println("***Mostrar los strings introuducidos***");
+         System.out.println("nuevo jdbc: " + nuevoJdbc);
+         System.out.println("nuevo usuario: " + nuevoUsuario);
+         System.out.println("nuevo clave: " + nuevoClave);
 
-      teclado.close();
-      
-   } catch (FileNotFoundException e) {
-      System.out.println(e.getMessage());
-   } finally {
-      if (lector!= null) lector.close();
-      
- }
+         Conexion nuevaConexion = new Conexion(nuevoJdbc, nuevoUsuario, nuevoClave);
 
-   
-   }
-   public static void insertarNuevaConfiguracion(Conexion conx, Properties properties){
+         insertarNuevaConfiguracion(nuevaConexion, propiedades);
+         System.out.println("***Visualizando con list los introuducidos con insertarNuevaConfiguracion***");
+         propiedades.list(System.out);
+
+         teclado.close();
+
+      } catch (FileNotFoundException e) {
+         System.out.println(e.getMessage());
+      } finally {
+         if (lector != null)
+            lector.close();
+
+      }
+
+      // NUEVO TRY-CATCH
+
+      FileOutputStream escritor = null;
+
+      try {
+         escritor = new FileOutputStream(new File(pathFichero));
+         propiedades.store(escritor, "Propiedades 23/04");
+
+      } catch (FileNotFoundException e) {
+         System.out.println(e.getMessage());
+      } finally {
+         if (escritor != null) {
+            escritor.close();
+
+         }
+      }
+
+      System.out.println("***Visualizando con leerFichConf***");
+      leerFichConf(pathFichero);
+   } // EO MAIN
+
+   public static void insertarNuevaConfiguracion(Conexion conx, Properties properties) {
       properties.setProperty("jdbc", conx.getJdbc());
       properties.setProperty("usuario", conx.getUsuario());
       properties.setProperty("clave", conx.getClave());
       properties.setProperty("nuevaClave", "nuevoValor");
    }
 
-   
-   
-}
+   public static void leerFichConf(String pathFichero) throws IOException {
+      Properties misProperties = new Properties();
+
+      BufferedReader lectorBuffered = null;
+
+      try {
+         lectorBuffered = new BufferedReader(new FileReader(new File(pathFichero)));
+         misProperties.load(lectorBuffered);
+         misProperties.list(System.out);
+
+      } catch (FileNotFoundException e) {
+         System.out.println(e.getMessage());
+      } finally {
+         if (lectorBuffered != null) {
+            lectorBuffered.close();
+         }
+      }
+   }
+
+}// EO CLASS
